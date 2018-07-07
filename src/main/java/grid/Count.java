1ea67286c7f313574e2a724750e73cbb83a69240
Xar83 <40188397+Xar83@users.noreply.github.com>
2018-07-07 11:32:41 -0600

    commiting

--- src/main/java/grid/Count.java
+++ src/main/java/grid/Count.java
@@ -1,25 +1,64 @@
 package grid;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.AsyncContext;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
 
 /**
  *
  * @author Ethgar
  */
-public class Count {
+
+@ServerEndpoint("/Count")
+public class Count{
    private static int cnt = 0;
-   
    public Count(){}
    
-   public int getCount(){
+   public int getCount()
+   {
        return cnt;
    }
    
-   public void addC(){
+   public void addC()
+   {
        cnt++;
    }
+   
+    @OnOpen
+    public void open(Session session) throws IOException 
+    {  
+      addC();
+      session.getBasicRemote().sendText(String.valueOf(cnt));
+    }
+
+    @OnClose
+    public void close(Session session) 
+    {
+        
+    }
+    
+    @OnError
+    public void onError(Throwable error) 
+    {
+        Logger.getLogger(Count.class.getName()).log(Level.SEVERE, null, error);
+    }
+    
+    
+    @OnMessage
+    public void handleMessage(String message, Session session) throws IOException 
+    {
+        session.getBasicRemote().sendText(message);
+    }
 }
