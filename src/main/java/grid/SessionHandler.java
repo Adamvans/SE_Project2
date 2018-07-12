1a874be7675e3817835177be7bcf7c266367a0f1
Xar83 <40188397+Xar83@users.noreply.github.com>
2018-07-11 20:34:01 -0600

    commiting

--- src/main/java/grid/SessionHandler.java
+++ src/main/java/grid/SessionHandler.java
@@ -6,13 +6,17 @@
 package grid;
 
 import com.aws.codestar.projecttemplates.Board;
+import com.aws.codestar.projecttemplates.Square;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.inject.Inject;
+import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.spi.JsonProvider;
 import javax.websocket.Session;
 
 /**
@@ -21,16 +25,25 @@ import javax.websocket.Session;
  */
 public class SessionHandler {
     private final Set<Session> sessions = new HashSet();
-    private final Board current = new Board();
-    
-    
+    private final Board gameBoard = new Board();
     
     public void addSession(Session session) 
     {
         sessions.add(session);
-        JsonObject addMessage = createAddMessage(gameBoard);
+        JsonObject addMessage = createMessage(gameBoard);
         sendToSession(session, addMessage);
         
+    }
+    
+    private JsonObject createAddMessage(Board B) {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        for(Square[] row: B.getBoard())
+        {
+            
+        }
+                
+        JsonObject addMessage = builder.build();
+        return addMessage;
     }
     
     public void removeSession(Session session) 
